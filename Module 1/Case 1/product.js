01aa1c45f1af48407d442ba241a599cf5744f31c
baothitran <baothitran.ntp@gmail.com>
2023-03-06 11:32:03 +0700

    update

--- Module 1/Case 1/product.js
+++ Module 1/Case 1/product.js
@@ -92,16 +92,6 @@ function searchByName() {
 
 }
 
-function getMaxId() {
-    let max = 0;
-    for (let i = 0; i < products.length; i++) {
-        if (products[i].id > max) {
-            max = products[i].id;
-        }
-    }
-    return max;
-}
-
 function selectProducts(productId) {
     if (checked_list.includes(productId)) {
         checked_list = checked_list.filter(function (id) {
@@ -139,25 +129,6 @@ function selectAllProducts() {
     console.log(checked_list);
 }
 
-// function add() {
-//     let image = document.getElementById("productid").value;
-//     let name = document.getElementById("name").value;
-//     let price = document.getElementById("price").value;
-//     let quantity = document.getElementById("quantity").value;
-//     let weight = document.getElementById("weight").value;
-
-//     if (name == "" || price == "" || quantity == "" || image == "" || weight == "") {
-//         alert("Thông tin không hợp lệ vui lòng nhập lại")
-//     } else {
-//         let maxId = productMaxId(products) + 1;
-//         let product = new Product(maxId, name, price, quantity, weight);
-
-//         products.push(product);
-//         drawProduct();
-//     }
-
-
-// }
 function add() {
     let name = document.getElementById('name').value;
     if (isEmpty(name)) {
@@ -190,6 +161,16 @@ function add() {
     renderTable();
     localStorage.setItem(products_tbl, JSON.stringify(products))
     resetAddForm();
+}
+
+function getMaxId() {
+    let max = 0;
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id > max) {
+            max = products[i].id;
+        }
+    }
+    return max;
 }
 
 function isEmpty(value) {
@@ -297,101 +278,5 @@ function sort(direction) {
 function start() {
     init();
     renderTable();
-    // renderSearchList();
 }
 start();
-
-// function Product(productid, image, name, quantity, weight, price) {
-//     this.productid = productid;
-//     this.image = image
-//     this.name = name;
-//     this.price = price;
-//     this.quantity = quantity;
-//     this.weight = weight;
-
-//     this.getQuantity = function () {
-//         return this.quantity;
-//     }
-//     this.setQuantity = function (quantity) {
-//         this.quantity = quantity;
-//     }
-//     this.getId = function () {
-//         return this.productid;
-//     }
-//     this.setId = function (productid) {
-//         this.productid = productid;
-//     }
-//     this.getName = function () {
-//         return this.name;
-//     }
-//     this.setName = function (name) {
-//         this.name = name;
-//     }
-//     this.getPrice = function () {
-//         return this.price;
-//     }
-//     this.setPrice = function (price) {
-//         this.price = price;
-//     }
-//     this.getImage = function () {
-//         return this.image;
-//     }
-//     this.setImage = function (image) {
-//         this.image = image;
-//     }
-//     this.getWeight = function () {
-//         return this.weight;
-//     }
-//     this.setWeight = function (weight) {
-//         this.weight = weight;
-//     }
-// }
-
-// function initData() {
-//     let p1 = new Product(1, "https://www.tofucute.com/images/10NL115-53_kitkat_bakable_custard_pudding_large.jpg", "Kit Kat Baked Custard Pudding", 12, 11.6, 4);
-//     let p2 = new Product(2, "https://www.tofucute.com/images/10NL115-57_kitkat_strawberry_mochi_large.jpg", "Kit Kat Strawberry Daifuku Mochi", 25, 9.9, 3);
-//     let p3 = new Product(3, "https://www.tofucute.com/images/10NL110-67_kitkat_setof2_strawberry_milk_large.jpg", "Kit Kat Strawberry Milk", 10, 9.9, 3);
-//     let p4 = new Product(4, "https://www.tofucute.com/images/10NL110-78_kitkat_banana_caramel_2022update_large.jpg", "Kit Kat Banana Caramel", 19, 9.9, 3);
-//     let p5 = new Product(5, "https://www.tofucute.com/images/10NL115-76_kit_kat_saveblueocean_large.jpg", "Kit Kat Save the Blue Ocean", 22, 11.6, 5);
-
-//     products = [p1, p2, p3, p4, p5];
-// }
-// function drawProduct() {
-//     let strProducts = products.map((product) => {
-//         return `
-//         <tr>
-//             <td><input class='check-box' id="checkbox_${product.productid}" name='checkbox_${product.productid}' type="checkbox" onchange='selectProducts(${product.productid})'></td>
-//             <td id='img_${product.productid}'  class="product-image-right"><img src="${product.image}" alt="" width="70"
-//             </td>
-//             <td id='name_${product.productid}'>${product.name}</td>
-//             <td id='quantity_${product.productid}'>${product.quantity}</td>
-//             <td id='weight_${product.productid}'>${product.weight} g</td>
-//             <td id='price_${product.productid}'>$ ${product.price}.00</td>
-//             <td>
-//             <i class="fa-solid fa-pen-to-square" onclick="handleEditItem(${product.productid})"></i>
-//             </td>
-//         </tr>
-//         `
-//     });
-//     document.getElementById("tbProducts").innerHTML = strProducts.join("");
-// }
-
-// function drawProductsFilter(productsFilter) {
-//     let strProducts = productsFilter.map((product) => {
-//         return `
-//         <tr>
-//             <td><input class='check-box' id="checkbox_${product.productid}" name='checkbox_${product.productid}' type="checkbox" onchange='selectProducts(${product.productid})'></td>
-//             <td id='img_${product.productid}'  class="product-image-right"><img src="${product.image}" alt="" width="70"
-//             </td>
-//             <td id='name_${product.productid}'>${product.name}</td>
-//             <td id='quantity_${product.productid}'>${product.quantity}</td>
-//             <td id='weight_${product.productid}'>${product.weight} g</td>
-//             <td id='price_${product.productid}'>$ ${product.price}.00</td>
-//             <td>
-//             <i class="fa-solid fa-pen-to-square" onclick="handleEditItem(${product.productid})"></i>
-//             </td>
-//         </tr>
-//         `
-//     });
-//     document.getElementById("tbProducts").innerHTML = strProducts.join("");
-// }
